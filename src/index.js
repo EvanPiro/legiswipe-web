4c0a926e2f16d5f4fcdf414e31f3206b71597d01
EvanPiro <dev.evanpiro@gmail.com>
2023-04-25 00:00:14 -0400

    config debug

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const apiKey = process.env.ELM_APP_API_KEY;
 const rawData = localStorage.getItem(key);
 const flags = { maybeModel: rawData && JSON.parse(rawData), apiKey };
 
+console.log(process.env);
+
 const app = Elm.Main.init({
   node: document.getElementById("root"),
   flags,
