0684c8297b9254f02fd82e99f74c4d7eb4442ec2
EvanPiro <dev.evanpiro@gmail.com>
2023-05-14 01:13:35 -0400

    add dynamodb backend

--- netlify/functions/log.ts
+++ netlify/functions/log.ts
@@ -1,4 +1,21 @@
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const region = "us-east-1";
+const tableName = "basic_data_service";
+const user = "legiswipe";
+
+const client = new DynamoDBClient({
+  region,
+  credentials: {
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
+  },
+});
 
 const handler: Handler = async (
   event: HandlerEvent,
@@ -8,8 +25,17 @@ const handler: Handler = async (
     return {
       statusCode: 405,
     };
-  console.log(event);
+  const command = new PutItemCommand({
+    TableName: tableName,
+    Item: marshall({
+      user,
+      timestamp: new Date().getTime().toString(),
+      ...event,
+      ...JSON.parse(event.body),
+    }),
+  });
 
+  await client.send(command);
   return {
     statusCode: 200,
   };
