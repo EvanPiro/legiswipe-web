d35e6b57dd6d0764269e21c6812d08d63972a9d8
EvanPiro <dev.evanpiro@gmail.com>
2023-05-31 22:05:28 -0400

    add demo login

---
+++ netlify/functions/redeam.ts
@@ -0,0 +1,16 @@
+import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const handler: Handler = async (
+  event: HandlerEvent,
+  context: HandlerContext
+) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(610),
+  };
+};
+
+export { handler };
